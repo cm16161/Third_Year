4ca934436501521168588a5e954c81c9907c7380
Chetan Mistry <cmistry98@gmail.com>
2019-02-09 21:34:44 -0600

    history almost done

--- Introduction_To_OS/Project_2a/wish.c
+++ Introduction_To_OS/Project_2a/wish.c
@@ -7,6 +7,7 @@
 
 char error_message[30] = "An error has occurred\n";
 int numberOfArgs = 32;
+int historyIndex = 0;
 
 void displayError(){
   write(STDERR_FILENO,error_message,strlen(error_message));
@@ -45,20 +46,41 @@ void cdHandler(char* args[numberOfArgs], int argNum){
     displayError();
   }
   else{
-    int success = 0;
-    success = chdir(args[0]);
+    int success = chdir(args[0]);
     if(success != 0){
       displayError();
     }
   }
 }
 
-void tokenHandler(char* command, char* args[numberOfArgs], int argNum, char* path){
-  if(strcmp(command,"exit")==0){
+void historyHandler(char* args[numberOfArgs], int argNum, char** history){
+  if(argNum >1){
+    displayError();
+  }
+  else{
+    if(argNum==0){
+      for(int i = 0; i<historyIndex-1;i++){
+        printf("%s",history[i]);
+      }
+    }
+    else{
+      int n = atoi(args[0]);
+      for(int i=historyIndex-1-n;i<historyIndex-1;i++){
+        printf("%s",history[i]);
+      }
+    }
+  }
+}
+
+void tokenHandler(char* command, char* args[numberOfArgs], int argNum, char* path, char** history){
+  if(strcmp(command,"exit") ==0){
     exitHandler(argNum);
   }
   else if(strcmp(command,"cd")==0){
     cdHandler(args, argNum);
+  }
+  else if(strcmp(command,"history")==0){
+    historyHandler(args,argNum,history);
   }
   else{
     char* executable = malloc(strlen(path)+1+strlen(command));
@@ -99,6 +121,13 @@ int main(int argc,char* argv[]){
   char *token;
   char *args[numberOfArgs];
   char *command;
+  int historySize =1;
+  char **history = calloc(historySize, sizeof(char*));
+  size_t argLength;
+  /* history[0]="TEST\n"; */
+  /* history[1]="WORLD\n"; */
+  /* history[2]="MOOO\n"; */
+  /* history= (char **)malloc(historySize * sizeof(char)); */
   buffer = (char *)malloc(buffsize * sizeof(char));
   if( buffer == NULL)
     {
@@ -106,21 +135,43 @@ int main(int argc,char* argv[]){
       exit(1);
     }
   while(!eof){
+    int noInput = 0;
     int count =0;
     printf("wish> ");
-    getline(&buffer, &buffsize, stdin);
+    argLength = getline(&buffer, &buffsize, stdin);
+    char line[argLength];
+    strcpy(line,buffer);
+    /* printf("%s",line); */
     token = strtok(buffer,delim);
-    command = token;
-    while(token != NULL){
-      token=strtok(NULL,delim);
-      args[count] = token;
-      count++;
+    if(token==NULL){
+      noInput=1;
     }
-    count--;
-    tokenHandler(command, args,count, path);
+    if(!noInput){
+      command = token;
+      history[historyIndex] = strdup(line);
+      historyIndex++;
+      /* for(int i =0;i<historyIndex;i++){ */
+        /* printf("%s",history[i]); */
+      /* } */
+      /* printf("HISTORYINDEX: %d\n",historyIndex); */
+      if(historyIndex == historySize){
+        historySize *=2;
+        /* printf("HISTORYSIZE: %d\n",historySize); */
+        /* history = (char **) realloc(history,historySize * sizeof(char)); */
+      }
+      while(token != NULL){
+        token=strtok(NULL,delim);
+        args[count] = token;
+        count++;
+      }
+      count--;
+      tokenHandler(command, args,count, path, history);
+    }
     eof= feof(stdin);
   }
-  exit(0);
+
+  
   return 0;
+  exit(0);
 }
 
